5ddaaa031f79a32e04246e17129713f907eb35d2
Somewhere <66011798+Somwere@users.noreply.github.com>
2021-03-27 23:19:23 +0200

    Init

---
+++ AdoNet_HW_2/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿
+namespace AdoNet_HW_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.getModyfiedValues = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.query9 = new System.Windows.Forms.Button();
+            this.query10 = new System.Windows.Forms.Button();
+            this.query11 = new System.Windows.Forms.Button();
+            this.query13 = new System.Windows.Forms.Button();
+            this.query14 = new System.Windows.Forms.Button();
+            this.query15 = new System.Windows.Forms.Button();
+            this.query12 = new System.Windows.Forms.Button();
+            this.query16 = new System.Windows.Forms.Button();
+            this.query2 = new System.Windows.Forms.Button();
+            this.query4 = new System.Windows.Forms.Button();
+            this.query3 = new System.Windows.Forms.Button();
+            this.query8 = new System.Windows.Forms.Button();
+            this.query7 = new System.Windows.Forms.Button();
+            this.query6 = new System.Windows.Forms.Button();
+            this.query1 = new System.Windows.Forms.Button();
+            this.query5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(169, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(656, 209);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // getModyfiedValues
+            // 
+            this.getModyfiedValues.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.getModyfiedValues.Location = new System.Drawing.Point(415, 485);
+            this.getModyfiedValues.Name = "getModyfiedValues";
+            this.getModyfiedValues.Size = new System.Drawing.Size(168, 51);
+            this.getModyfiedValues.TabIndex = 1;
+            this.getModyfiedValues.Text = "Get Modyfied Values";
+            this.getModyfiedValues.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(169, 260);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(656, 209);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // query9
+            // 
+            this.query9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query9.Location = new System.Drawing.Point(849, 30);
+            this.query9.Name = "query9";
+            this.query9.Size = new System.Drawing.Size(75, 23);
+            this.query9.TabIndex = 3;
+            this.query9.Text = "Query 9";
+            this.query9.UseVisualStyleBackColor = true;
+            this.query9.Click += new System.EventHandler(this.query9_Click);
+            // 
+            // query10
+            // 
+            this.query10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query10.Location = new System.Drawing.Point(849, 70);
+            this.query10.Name = "query10";
+            this.query10.Size = new System.Drawing.Size(75, 23);
+            this.query10.TabIndex = 4;
+            this.query10.Text = "Query 10";
+            this.query10.UseVisualStyleBackColor = true;
+            this.query10.Click += new System.EventHandler(this.query10_Click);
+            // 
+            // query11
+            // 
+            this.query11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query11.Location = new System.Drawing.Point(849, 114);
+            this.query11.Name = "query11";
+            this.query11.Size = new System.Drawing.Size(75, 23);
+            this.query11.TabIndex = 5;
+            this.query11.Text = "Query 11";
+            this.query11.UseVisualStyleBackColor = true;
+            this.query11.Click += new System.EventHandler(this.query11_Click);
+            // 
+            // query13
+            // 
+            this.query13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query13.Location = new System.Drawing.Point(849, 201);
+            this.query13.Name = "query13";
+            this.query13.Size = new System.Drawing.Size(75, 23);
+            this.query13.TabIndex = 6;
+            this.query13.Text = "Query 13";
+            this.query13.UseVisualStyleBackColor = true;
+            this.query13.Click += new System.EventHandler(this.query13_Click);
+            // 
+            // query14
+            // 
+            this.query14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query14.Location = new System.Drawing.Point(849, 245);
+            this.query14.Name = "query14";
+            this.query14.Size = new System.Drawing.Size(75, 23);
+            this.query14.TabIndex = 7;
+            this.query14.Text = "Query 14";
+            this.query14.UseVisualStyleBackColor = true;
+            this.query14.Click += new System.EventHandler(this.query14_Click);
+            // 
+            // query15
+            // 
+            this.query15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query15.Location = new System.Drawing.Point(849, 290);
+            this.query15.Name = "query15";
+            this.query15.Size = new System.Drawing.Size(75, 23);
+            this.query15.TabIndex = 8;
+            this.query15.Text = "Query 15";
+            this.query15.UseVisualStyleBackColor = true;
+            this.query15.Click += new System.EventHandler(this.query15_Click);
+            // 
+            // query12
+            // 
+            this.query12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query12.Location = new System.Drawing.Point(849, 156);
+            this.query12.Name = "query12";
+            this.query12.Size = new System.Drawing.Size(75, 23);
+            this.query12.TabIndex = 16;
+            this.query12.Text = "Query 12";
+            this.query12.UseVisualStyleBackColor = true;
+            this.query12.Click += new System.EventHandler(this.query12_Click);
+            // 
+            // query16
+            // 
+            this.query16.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query16.Location = new System.Drawing.Point(849, 335);
+            this.query16.Name = "query16";
+            this.query16.Size = new System.Drawing.Size(75, 23);
+            this.query16.TabIndex = 18;
+            this.query16.Text = "Query 16";
+            this.query16.UseVisualStyleBackColor = true;
+            this.query16.Click += new System.EventHandler(this.query16_Click);
+            // 
+            // query2
+            // 
+            this.query2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query2.Location = new System.Drawing.Point(60, 70);
+            this.query2.Name = "query2";
+            this.query2.Size = new System.Drawing.Size(75, 23);
+            this.query2.TabIndex = 9;
+            this.query2.Text = "Query 2";
+            this.query2.UseVisualStyleBackColor = true;
+            this.query2.Click += new System.EventHandler(this.query2_Click);
+            // 
+            // query4
+            // 
+            this.query4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query4.Location = new System.Drawing.Point(60, 156);
+            this.query4.Name = "query4";
+            this.query4.Size = new System.Drawing.Size(75, 23);
+            this.query4.TabIndex = 10;
+            this.query4.Text = "Query 4";
+            this.query4.UseVisualStyleBackColor = true;
+            this.query4.Click += new System.EventHandler(this.query4_Click);
+            // 
+            // query3
+            // 
+            this.query3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query3.Location = new System.Drawing.Point(60, 114);
+            this.query3.Name = "query3";
+            this.query3.Size = new System.Drawing.Size(75, 23);
+            this.query3.TabIndex = 11;
+            this.query3.Text = "Query 3";
+            this.query3.UseVisualStyleBackColor = true;
+            this.query3.Click += new System.EventHandler(this.query3_Click);
+            // 
+            // query8
+            // 
+            this.query8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query8.Location = new System.Drawing.Point(60, 335);
+            this.query8.Name = "query8";
+            this.query8.Size = new System.Drawing.Size(75, 23);
+            this.query8.TabIndex = 12;
+            this.query8.Text = "Query 8";
+            this.query8.UseVisualStyleBackColor = true;
+            this.query8.Click += new System.EventHandler(this.query8_Click);
+            // 
+            // query7
+            // 
+            this.query7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query7.Location = new System.Drawing.Point(60, 290);
+            this.query7.Name = "query7";
+            this.query7.Size = new System.Drawing.Size(75, 23);
+            this.query7.TabIndex = 13;
+            this.query7.Text = "Query 7";
+            this.query7.UseVisualStyleBackColor = true;
+            this.query7.Click += new System.EventHandler(this.query7_Click);
+            // 
+            // query6
+            // 
+            this.query6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query6.Location = new System.Drawing.Point(60, 245);
+            this.query6.Name = "query6";
+            this.query6.Size = new System.Drawing.Size(75, 23);
+            this.query6.TabIndex = 14;
+            this.query6.Text = "Query 6";
+            this.query6.UseVisualStyleBackColor = true;
+            this.query6.Click += new System.EventHandler(this.query6_Click);
+            // 
+            // query1
+            // 
+            this.query1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query1.Location = new System.Drawing.Point(60, 30);
+            this.query1.Name = "query1";
+            this.query1.Size = new System.Drawing.Size(75, 23);
+            this.query1.TabIndex = 15;
+            this.query1.Text = "Query 1";
+            this.query1.UseVisualStyleBackColor = true;
+            this.query1.Click += new System.EventHandler(this.query1_Click);
+            // 
+            // query5
+            // 
+            this.query5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query5.Location = new System.Drawing.Point(60, 201);
+            this.query5.Name = "query5";
+            this.query5.Size = new System.Drawing.Size(75, 23);
+            this.query5.TabIndex = 17;
+            this.query5.Text = "Query 5";
+            this.query5.UseVisualStyleBackColor = true;
+            this.query5.Click += new System.EventHandler(this.query5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = global::AdoNet_HW_2.Properties.Resources.фон;
+            this.ClientSize = new System.Drawing.Size(977, 552);
+            this.Controls.Add(this.query16);
+            this.Controls.Add(this.query5);
+            this.Controls.Add(this.query12);
+            this.Controls.Add(this.query1);
+            this.Controls.Add(this.query6);
+            this.Controls.Add(this.query7);
+            this.Controls.Add(this.query8);
+            this.Controls.Add(this.query3);
+            this.Controls.Add(this.query4);
+            this.Controls.Add(this.query2);
+            this.Controls.Add(this.query15);
+            this.Controls.Add(this.query14);
+            this.Controls.Add(this.query13);
+            this.Controls.Add(this.query11);
+            this.Controls.Add(this.query10);
+            this.Controls.Add(this.query9);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.getModyfiedValues);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Database Values Change";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button getModyfiedValues;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button query9;
+        private System.Windows.Forms.Button query10;
+        private System.Windows.Forms.Button query11;
+        private System.Windows.Forms.Button query13;
+        private System.Windows.Forms.Button query14;
+        private System.Windows.Forms.Button query15;
+        private System.Windows.Forms.Button query12;
+        private System.Windows.Forms.Button query16;
+        private System.Windows.Forms.Button query2;
+        private System.Windows.Forms.Button query4;
+        private System.Windows.Forms.Button query3;
+        private System.Windows.Forms.Button query8;
+        private System.Windows.Forms.Button query7;
+        private System.Windows.Forms.Button query6;
+        private System.Windows.Forms.Button query1;
+        private System.Windows.Forms.Button query5;
+    }
+}
+
---
+++ AdoNet_HW_2/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AdoNet_HW_2
+{
+    public partial class Form1 : Form
+    {
+        public string connection = @"Data Source = SQL5103.site4now.net; Initial Catalog = DB_A7168B_kuvaevsql; User Id = DB_A7168B_kuvaevsql_admin; Password=qwerty009";
+        SqlConnection sqlConnection = null;
+        SqlDataAdapter sqlDateAdapter = null;
+        DataSet dataSet = null;
+        SqlCommandBuilder sqlCommanBulder = null;
+        public Form1()
+        {
+            InitializeComponent();
+            sqlConnection = new SqlConnection(connection);
+            getModyfiedValues.Click += BtnModifiedValues_Click; 
+            try
+            {
+                string query = "select * from [Coffe Shop]";
+                dataGridView1.DataSource = null;
+                dataSet = new DataSet();
+                sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+                sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+                sqlDateAdapter.Fill(dataSet);
+                dataGridView1.DataSource = dataSet.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sqlConnection != null)
+                    sqlConnection.Close();
+            }
+        }
+        private void BtnModifiedValues_Click(object sender, EventArgs e)
+        {
+            dataGridView2.DataSource = null;
+            dataGridView2.DataSource = dataSet.Tables[0].GetChanges(DataRowState.Modified);
+        }
+        // Показать информацию о кофе в описании, которых встречается упоминание вишни
+        private void query1_Click(object sender, EventArgs e)
+        {
+            string query = "select [Sort Name], [About] from [Coffe Shop] where [About] like 'вишня'";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+        // Показать информацию о кофе с себестоимостью в указанном диапазоне
+        private void query2_Click(object sender, EventArgs e)
+        {
+            string query = "select [Sort Name], [About] from [Coffe Shop] where [Price] between 1000 and 2000";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+        // Показать информацию о кофе с количеством грамм в указанном диапазоне
+        private void query3_Click(object sender, EventArgs e)
+        {
+            string query = "select [Sort Name], [About] from [Coffe Shop] where [Weight] between 300 and 400";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+        // Показать информацию о кофе из указанных стран
+        private void query4_Click(object sender, EventArgs e)
+        {
+            string query = "select [Sort Name], [About] from [Coffe Shop] where [Country] = 'Италия'";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+        #region Отобразить название страны происхождения и количество сортов кофе из этой страны
+        private void query5_Click(object sender, EventArgs e)
+        {
+            string query = "select count([Sort Name]) from [Coffe Shop] where [Country] = 'Италия'";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+
+        private void query6_Click(object sender, EventArgs e)
+        {
+            string query = "select count([Sort Name]) from [Coffe Shop] where [Country] = 'Индонезия'";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+
+        private void query7_Click(object sender, EventArgs e)
+        {
+            string query = "select count([Sort Name]) from [Coffe Shop] where [Country] = 'Бразилия'";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+        #endregion
+        #region Отобразить среднее количество грамм кофе по каждой стране
+        private void query8_Click(object sender, EventArgs e)
+        {
+            string query = "select avg([Weight]) from [Coffe Shop] where [Country] = 'Италия'";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+
+        private void query9_Click(object sender, EventArgs e)
+        {
+            string query = "select avg([Weight]) from [Coffe Shop] where [Country] = 'Индонезия'";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+
+        private void query10_Click(object sender, EventArgs e)
+        {
+            string query = "select avg([Weight]) from [Coffe Shop] where [Country] = 'Бразилия'";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+        #endregion
+        #region Показать три самых дешевых сорта кофе по всем странам
+        private void query11_Click(object sender, EventArgs e)
+        {
+            string query = "select top(3) * from [Coffe Shop] where [Country] = 'Италия' order by [Price] asc";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+
+        private void query12_Click(object sender, EventArgs e)
+        {
+            string query = "select top(3) * from [Coffe Shop] where [Country] = 'Индонезия' order by [Price] asc";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+
+        private void query13_Click(object sender, EventArgs e)
+        {
+            string query = "select top(3) * from [Coffe Shop] where [Country] = 'Бразилия' order by [Price] asc";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+        #endregion
+        #region Показать три самых дорогих сорта кофе по всем странам
+        private void query14_Click(object sender, EventArgs e)
+        {
+            string query = "select top(3) * from [Coffe Shop] where [Country] = 'Италия' order by [Price] desc";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+
+        private void query15_Click(object sender, EventArgs e)
+        {
+            string query = "select top(3) * from [Coffe Shop] where [Country] = 'Индонезия' order by [Price] desc";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+
+        private void query16_Click(object sender, EventArgs e)
+        {
+            string query = "select top(3) * from [Coffe Shop] where [Country] = 'Бразилия' order by [Price] desc";
+            dataGridView2.DataSource = null;
+            dataSet = new DataSet();
+            sqlDateAdapter = new SqlDataAdapter(query, sqlConnection);
+            sqlCommanBulder = new SqlCommandBuilder(sqlDateAdapter);
+            sqlDateAdapter.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+        }
+        #endregion
+        private void clear_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Rows.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
